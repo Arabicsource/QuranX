17e13ed80db27c98a906d65cea78e01c8a49a22f
The Rationalizer <therationaliser@gmail.com>
2017-06-25 17:56:44 +0100

    Set up dependency injection + list chapters

---
+++ QuranX.Website/Controllers/QuranController.cs
@@ -0,0 +1,21 @@
+﻿using QuranX.DomainClasses.Services;
+using System.Web.Mvc;
+
+namespace QuranX.Website.Controllers
+{
+    public class QuranController : Controller
+    {
+        private readonly ChapterRepository ChapterRepository;
+
+        public QuranController(ChapterRepository chapterRepository)
+        {
+            ChapterRepository = chapterRepository;
+        }
+
+        // GET: Quran
+        public ActionResult Chapters()
+        {
+            return View(ChapterRepository.All());
+        }
+    }
+}
